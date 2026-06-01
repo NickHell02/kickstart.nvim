547e5cfadf7f5109f6bdf6975bd11cb11250599c
NickHell02 <nicholashell02@gmail.com>
2026-06-01 15:33:40 +0100

    disabled auto comments cause they're dumb

--- init.lua
+++ init.lua
@@ -97,6 +97,8 @@ do
   --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
+  vim.cmd 'autocmd BufEnter * set formatoptions-=cro'
+  vim.cmd 'autocmd BufEnter * setlocal formatoptions-=cro'
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = false
